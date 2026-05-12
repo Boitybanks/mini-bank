bfbc9f92d9f9f1c9e51475145f0628437a93b31d
Boitumelo Mbhele <bombhswgc025@student.wethinkcode.co.za>
2026-05-12 14:56:53 +0200

    Feat: Add Interest calculator method

--- src/Account.java
+++ src/Account.java
@@ -28,7 +28,7 @@ public class Account{
                     balance = balance - amount;
                     previousTransaction = -amount;
                     System.out.println("Widrawal Successful!");
-                    System.out.println("New Balance is: " + balance);
+                    System.out.println("New Balance is: " + "R" +balance);
 
                 } else{
                     System.out.println("Please enter amount greater than 0");
@@ -36,6 +36,27 @@ public class Account{
                     System.out.println("Insufficient Funds, cannot complete withdrawal");
                 }
             }
+        }
+    // Getting previous transaction
+    void getPreviousTransaction(){
+            if(previousTransaction > 0){
+                System.out.println("Deposited: " + previousTransaction);
+
+            } else if(previousTransaction < 0){
+                System.out.println("Withdrawn: " + previousTransaction);
+
+            } else {
+                System.out.println("No Transaction Occurred");
+            }
+
+        }
+     // Calculating the interest method
+     void calculateInterest(int years){
+            double interestRate = .0185; // constant rate
+            double newBalance = ( balance * interestRate* years) + balance; // Years is received as a parameter
+            System.out.println("The current interest rate is " + (100 * interestRate) + "%");
+            System.out.println("After " + years + "years, your balance will be: " + "R"+ newBalance );
+
         }
     }
 }
